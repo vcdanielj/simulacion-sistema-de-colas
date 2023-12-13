7ad7f14967226db8ed2ed8e06751ad16436ba809
Daniel <vcdanielj@gmail.com>
2023-12-12 23:29:47 -0400

    Adding templates

--- src/app.py
+++ src/app.py
@@ -8,5 +8,15 @@ def home():
     return render_template("/home.html")
 
 
+@app.route("/colas-con-servidores-finitos")
+def exercise1():
+    return render_template("/exercise5.html")
+
+
+@app.route("/colas-con-tiempos-de-servicio-variables")
+def exercise2():
+    return render_template("/exercise6.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
